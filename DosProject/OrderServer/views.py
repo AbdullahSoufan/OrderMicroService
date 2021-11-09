10f5a4ff0b712d7a1082887663813c2a7987ed3d
AbdullahSoufan <Abdullah.Soufan@hotmail.com>
2021-11-09 21:36:57 +0200

    Init_commit

--- DosProject/OrderServer/views.py
+++ DosProject/OrderServer/views.py
@@ -12,12 +12,3 @@ class bookList(APIView) :
     def put(self, request, id):
         books_with_specific_topic = requests.get('http://127.0.0.1:8080/info/'+str(id)+'/', params=request.GET, timeout=5)
         data = books_with_specific_topic.text
-        jsondata = json.loads(data)
-
-        quantity = jsondata[0]['quantity']
-        if quantity > 0:
-            requests.put('http://127.0.0.1:8080/decrease/'+str(id)+'/', timeout=5)
-            return HttpResponse('The book has been purchased successfully')
-        else:
-            return HttpResponse('There are no books left')
-
